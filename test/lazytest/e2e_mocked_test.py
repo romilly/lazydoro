8112a380c9311f7218bc42ce66b0aaa54760cd7e
Romilly Cocking <romilly.cocking@gmail.com>
2020-01-15 12:53:49 +0000

    new e2e test framework

--- test/lazytest/e2e_mocked_test.py
+++ test/lazytest/e2e_mocked_test.py
@@ -1,11 +1,16 @@
+from abc import ABC, abstractmethod
 from collections import defaultdict
 from unittest import TestCase
 
 from hamcrest import assert_that, equal_to
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+RED = 'Red'
+GREEN = 'Green'
+BLUE = 'Blue'
+YELLOW = 'Yellow'
+
+DURATION = 10
+BREAK = 3
 
 
 class AbstractClock():
@@ -23,7 +28,11 @@ class MockClock(AbstractClock):
         self.count += 1
         if self.count in self.assertions:
             for assertion in self.assertions[self.count]:
-                assertion() # run the relevant function
+                try:
+                    assertion() # run the relevant function
+                except Exception as e:
+                    print('at time %d' % self.count)
+                    raise e
         return self.count <= self.limit
 
     def at(self, time, *fns):
@@ -44,12 +53,16 @@ class MockTofSensor:
         self._distance = 1000
 
 
-class AbstractBuzzer:
+class AbstractBuzzer(ABC):
+    @abstractmethod
     def buzz(self, status):
         pass
 
 
 class MockBuzzer(AbstractBuzzer):
+    def buzz(self, status: bool):
+        self.buzzing = status
+
     def __init__(self):
         self.buzzing = False
 
@@ -65,22 +78,45 @@ class MockLed(object):
         self.color = (0, 0, 0)
 
 
-class State:
-    def update(self, range):
+class State(ABC):
+    @abstractmethod
+    def update(self, person_there: bool) -> ('State', bool, str):
         pass
 
 
+class Resting(State):
+    def __init__(self, duration):
+        self.ticks = 0
+        self.duration = duration
+
+    def update(self, person_there: bool) -> ('State', bool, str):
+        self.ticks += 1
+        if person_there:
+            return PomodoroRunning(DURATION), False, GREEN
+        if self.ticks < self.duration:
+            return self, True, YELLOW
+        return self, False, YELLOW
+
+
 class PomodoroRunning(State):
-    pass
+    def __init__(self, duration):
+        self.ticks = 0
+        self.duration = duration
 
-
-pomodoro_running = PomodoroRunning()
+    def update(self, person_there) -> ('State', bool, str):
+        self.ticks += 1
+        if not person_there:
+            return Resting(BREAK), False, YELLOW
+        if self.ticks < self.duration:
+            return self, False, GREEN
+        else:
+            return self, True, RED
 
 
 class Waiting(State):
-    def update(self, distance):
-        if distance < 1000:
-            return pomodoro_running, False, GREEN
+    def update(self, person_there: bool):
+        if person_there:
+            return PomodoroRunning(DURATION), False, GREEN
         else:
             return self, False, BLUE
 
@@ -97,15 +133,12 @@ class PomodoroTimer:
         state = Waiting()
         self.led.color = BLUE
         while self.clock.tick():
-            distance = self.tof_sensor.distance()
-            (state, buzzing, colour) = state.update(distance)
+            (state, buzzing, colour) = state.update(self.person_there())
             self.buzzer.buzz(buzzing)
             self.led.color = colour
 
-
-class Script:
-    def __init__(self, *events):
-        self._events = events
+    def person_there(self):
+        return self.tof_sensor.distance() < 400
 
 
 class LazyTest(TestCase):
@@ -119,19 +152,34 @@ class LazyTest(TestCase):
     def test_can_run_a_single_pomodoro(self):
         self.clock.at(1, self.buzzer_is_quiet)
         self.clock.at(1, self.led_is_blue)
+        self.clock.at(2, self.person_appears)
+        self.clock.at(3, self.led_is_green)
+        self.clock.at(DURATION+2, self.buzzer_is_quiet, self.led_is_green) # pomodoro started at time 3
+        self.clock.at(DURATION+3, self.buzzer_is_buzzing, self.led_is_red)
+        self.clock.at(DURATION+4, self.person_is_absent)
+        self.clock.at(DURATION+5, self.buzzer_is_quiet, self.led_is_yellow)
+
+
         self.pom.run()
 
     def buzzer_is_quiet(self):
         assert_that(not self.buzzer.buzzing,'buzzer is not quiet')
 
+    def buzzer_is_buzzing(self):
+        assert_that(self.buzzer.buzzing,'buzzer is quiet')
+
     def led_is_blue(self):
         assert_that(self.led.color, equal_to(BLUE))
 
+    def led_is_red(self):
+        assert_that(self.led.color, equal_to(RED))
 
+    def led_is_green(self):
+        assert_that(self.led.color, equal_to(GREEN))
 
+    def led_is_yellow(self):
+        assert_that(self.led.color, equal_to(YELLOW))
 
-    def wait(self, minutes, seconds=0):
-        pass
 
     def person_appears(self):
         self.tof_sensor.person_is_present()
