ddfeb06ef95bc15d5463dda5af9da2fe437ee93c
Romilly Cocking <romilly.cocking@gmail.com>
2020-01-24 13:09:42 +0000

    Removed redundant code.

--- src/lazy_zero.py
+++ src/lazy_zero.py
@@ -3,13 +3,12 @@ import sys
 sys.path.append('.') # ugh!
 from lazydoro.pi.blinkt_leds import BlinktLEDs
 from lazydoro.pi.hat_clock import HatClock
-from lazydoro.pi.lazy_oo import Schedule, PomodoroTimer, Clock
+from lazydoro.pi.lazy_oo import Schedule, PomodoroTimer
 from lazydoro.pi.pwmbuzzer import PwmBuzzer
 from lazydoro.pi.vl53l0x import VL53L0XToF
 
 
 if __name__ == '__main__':
-    time_units = 60 if len(sys.argv) < 2 else 1 # to run a demo, just append 'demo' after the script name
     tof_sensor = VL53L0XToF()
     schedule = Schedule(25*60, 5*60, 4, 4) if len(sys.argv) < 2 else Schedule(16, 8, 4, 4)
     clock = HatClock()
